7681c591d7478026cdfc76fa7ffac4571f2c23be
Juan Diego Carballo <juandiegocarballo2701@gmail.com>
2024-08-20 23:03:40 -0300

    feat: create folders for chapter 1 and 2 and create a variable

---
+++ variables/variables/target/debug/deps/variables-87379d31dff2b23e.d
@@ -0,0 +1,5 @@
+/Users/juandiegocv/Documents/rust-learning/variables/variables/target/debug/deps/variables-87379d31dff2b23e: src/main.rs
+
+/Users/juandiegocv/Documents/rust-learning/variables/variables/target/debug/deps/variables-87379d31dff2b23e.d: src/main.rs
+
+src/main.rs:
---
+++ variables/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/juandiegocv/Documents/rust-learning/variables/variables/target/debug/variables: /Users/juandiegocv/Documents/rust-learning/variables/variables/src/main.rs
