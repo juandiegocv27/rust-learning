4e49a2c2c7e97b7102fb8828b19eb4671478bd80
Juan Diego Carballo <juandiegocarballo2701@gmail.com>
2024-08-24 16:37:01 -0300

    feat: add functions

---
+++ functions/target/debug/deps/functions-3e6d97b414ef008f.d
@@ -0,0 +1,5 @@
+/Users/juandiegocv/Documents/rust-learning/functions/target/debug/deps/functions-3e6d97b414ef008f: src/main.rs
+
+/Users/juandiegocv/Documents/rust-learning/functions/target/debug/deps/functions-3e6d97b414ef008f.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/juandiegocv/Documents/rust-learning/functions/target/debug/functions: /Users/juandiegocv/Documents/rust-learning/functions/src/main.rs
