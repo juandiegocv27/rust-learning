6ccd7293bb2191b5f11d8f00ba9604f30743a4be
juandiegocv27 <juandiegocarballo2701@gmail.com>
2024-08-29 22:50:18 -0600

    feat(chapter4): add first steps in ownership

---
+++ chapter4/ownership1/target/debug/deps/ownership1-754553f2644c4bc3.d
@@ -0,0 +1,5 @@
+/Users/juandicv_/Documents/rust-learning/chapter4/ownership1/target/debug/deps/ownership1-754553f2644c4bc3: src/main.rs
+
+/Users/juandicv_/Documents/rust-learning/chapter4/ownership1/target/debug/deps/ownership1-754553f2644c4bc3.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/ownership1/target/debug/ownership1.d
@@ -0,0 +1,1 @@
+/Users/juandicv_/Documents/rust-learning/chapter4/ownership1/target/debug/ownership1: /Users/juandicv_/Documents/rust-learning/chapter4/ownership1/src/main.rs
